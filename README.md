64d0f482e0755c52ecb763c119eb928d40bb6f43
kennyfrc <kennyfrc@Kenns-MacBook-Pro.local>
2019-06-11 00:32:08 +0800

    OOP

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a simple implementation of a todolist in PHP meant for learning purposes.
 
 Version 1: Just playing with data structures, array methods, and prints
+Version 2: Used functions
+Version 3: OOP
--- todo.php
+++ todo.php
@@ -1,39 +1,41 @@
 <?php
 
-function displayTodos($todos) {
-	print_r($todos);
+class todo_list {
+	public $todos = array();
+
+	function display_todos() {
+		print_r($this->todos);
+	}
+
+	function add_todo($todo) {
+		array_push($this->todos, $todo);
+	}
+
+	function change_todo($idx, $todo) {
+		array_splice($this->todos, $idx, 1, $todo);
+	}
+
+	function delete_todo($idx) {
+		array_splice($this->todos, $idx, 1);
+	}
 }
 
-function addTodo($todos, $todo) {
-	array_push($todos, $todo);
-	displayTodos($todos);
-}
 
-function changeTodo($todos, $idx, $todo) {
-	array_splice($todos, $idx, 1, $todo);
-	displayTodos($todos);
-}
+print_r("initialized todos");
+$todos = new todo_list;
 
-function deleteTodo($todos, $idx) {
-	array_splice($todos, $idx, 1);
-	displayTodos($todos);
-}
+print_r("add todo\n");
+$todos->add_todo("plan");
+$todos->add_todo("execute");
+$todos->add_todo("win");
+$todos->display_todos();
 
-// initialized todos
-$todos = array("plan", "execute", "win");
-echo "Initialized Todo\n";
-displayTodos($todos);
+print_r("change todo\n");
+$todos->change_todo(0, "assess");
+$todos->display_todos();
 
-// added a todo
-echo "Added celebrate\n";
-addTodo($todos, "celebrate");
+print_r("delete todo\n");
+$todos->delete_todo(2);
+$todos->display_todos();
 
-
-// change a todo
-echo "Changed a todo\n";
-changeTodo($todos, 0, "assess");
-
-// delete a todo
-echo "Deleted a todo\n";
-deleteTodo($todos, 2);
 ?>
